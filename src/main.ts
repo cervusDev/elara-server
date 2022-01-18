923cd3ab6acf871fb70a1055f0ab193f471a9688
Gustavo Henrique <gustavo.cervus@gmail.com>
2022-01-17 21:45:39 -0300

    🔧 initial configs in main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      // remove o objeto não validator por um decorator
+      whitelist: true,
+      // lança uma exceção relacionada a requisição
+      errorHttpStatusCode: 422,
+      // transforma o tipo primitivo de acordo com o DTO
+      transform: true,
+    }),
+  );
+
   await app.listen(3000);
 }
 bootstrap();
