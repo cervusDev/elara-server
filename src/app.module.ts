2fcafb56fe43936618831b5b3fe185f8221db19a
Gustavo Henrique <gustavo.cervus@gmail.com>
2022-01-18 23:12:05 -0300

    🔧 config shared layer

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,17 @@
-import { Module } from '@nestjs/common';
+import { Global, Module } from '@nestjs/common';
+import { HttpModule } from '@nestjs/axios';
+import { ConfigModule } from '@nestjs/config';
+import { PrismaModule } from './infra/database/prisma/prisma.module';
 
+@Global()
 @Module({
-  imports: [],
-  controllers: [],
-  providers: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    PrismaModule,
+    HttpModule,
+  ],
+  exports: [HttpModule],
 })
 export class AppModule {}
