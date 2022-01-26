536af93788125b53d1cc2d02911f2dc43ee88939
Gustavo Henrique <gustavo.cervus@gmail.com>
2022-01-25 21:12:57 -0300

    ✨ implements match decorator to password

--- src/presentation/rest/usuario/dto/create-user.dto.ts
+++ src/presentation/rest/usuario/dto/create-user.dto.ts
@@ -1,4 +1,10 @@
-import { IsEmail, IsNotEmpty, IsOptional, IsString } from 'class-validator';
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  Matches,
+} from 'class-validator';
 
 export class CreateUsuarioDto {
   @IsString()
@@ -7,6 +13,9 @@ export class CreateUsuarioDto {
   email: string;
 
   @IsString()
+  @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
+    message: 'senha muito fraca, escolha uma melhor!',
+  })
   @IsNotEmpty()
   password: string;
 
