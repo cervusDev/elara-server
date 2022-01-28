0bbbebcd0c099c4d8adc2815b97a3596569ee741
Gustavo Henrique <gustavo.cervus@gmail.com>
2022-01-28 00:07:40 -0300

    ✨ implements decorator notExist

--- src/decorators/not-exist.rules.ts
+++ src/decorators/not-exist.rules.ts
@@ -1,7 +1,60 @@
-// interface Data {
-//     table: string;
-//     field: string;
+import {
+  registerDecorator,
+  ValidationArguments,
+  ValidationOptions,
+  ValidatorConstraint,
+  ValidatorConstraintInterface,
+} from 'class-validator';
+import { PrismaService } from 'src/infra/database/prisma/prisma.service';
 
-// }
+interface Data {
+  table: string;
+  field: string;
+  validationOptions: ValidationOptions;
+}
 
-// export function NotExist({}) {}
+export function NotExist({ table, field, validationOptions }: Data) {
+  return function (object: any, propertyName: string) {
+    registerDecorator({
+      name: 'NotExists',
+      constraints: [table, field],
+      target: object.constructor,
+      propertyName,
+      options: validationOptions,
+      validator: NotExisRule,
+    });
+  };
+}
+
+@ValidatorConstraint({ name: 'NotExists', async: true })
+export class NotExisRule implements ValidatorConstraintInterface {
+  async validate(value: string, args: ValidationArguments): Promise<boolean> {
+    if (!value) return false;
+
+    try {
+      const [table, field] = args.constraints;
+
+      const prisma = new PrismaService();
+      await prisma.$connect();
+
+      const result = await prisma.$queryRawUnsafe<[]>(
+        `SELECT * FROM ${table} WHERE ${field} = ?`,
+        value,
+      );
+
+      await prisma.$disconnect();
+
+      if (result.length === 0) {
+        return true;
+      }
+    } catch (err) {
+      return false;
+    }
+  }
+  defaultMessage?(args: ValidationArguments) {
+    if (!args.value) {
+      return `${args.property} is required`;
+    }
+    return `${args.property} already exists`;
+  }
+}
